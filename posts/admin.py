8cc93a508e8200c954ecf355e009acae9bd35163
vujke <stefanvujovic93@gmail.com>
2016-07-25 15:29:43 +0200

    dodat model u admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from posts.models import Post
+
+# MODEL ADMIN nije uvek neophodan, on se koristi samo kada je potrebno da se prosiri ono sto daje default:
+#from posts.models import Post
+#admin.site.register(Post)
+admin.site.register(Post)
