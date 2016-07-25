eb7dca1f3c147692915981604c8c64f3d652f11f
vujke <stefanvujovic93@gmail.com>
2016-07-25 14:59:52 +0200

    dodat prvi model

--- posts/models.py
+++ posts/models.py
@@ -5,4 +5,10 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
