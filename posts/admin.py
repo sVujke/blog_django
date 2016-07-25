1d36d97b4e39e4db4fdb1049a1946c5e6f503fe1
vujke <stefanvujovic93@gmail.com>
2016-07-25 16:03:35 +0200

    admin customized

--- posts/admin.py
+++ posts/admin.py
@@ -6,4 +6,14 @@ from posts.models import Post
 # MODEL ADMIN nije uvek neophodan, on se koristi samo kada je potrebno da se prosiri ono sto daje default:
 #from posts.models import Post
 #admin.site.register(Post)
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "timestamp", "updated", "content"]
+    list_display_links = ["updated"]
+    list_filter = ["updated", "timestamp", "title"]
+    list_editable = ["title"]
+    search_fields = ["title", "content"]
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostAdmin)
