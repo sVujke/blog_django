669d95a87e180397036de563477c1cb26a6221cc
vujke <stefanvujovic93@gmail.com>
2016-07-25 17:00:09 +0200

    crud template

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 #from django.contrib import admin
 import posts
-
+from . import  views # from . means from current directory
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'$',"posts.views.posts_home"),
+    url(r'^$',"posts.views.posts_home"),
+    url(r'^create/$', "posts.views.post_create"),
+    url(r'^detail/$', "posts.views.post_detail"),
+    url(r'^update/$', "posts.views.post_update"),
+    url(r'^delete/$', "posts.views.post_delete"),
 ]
